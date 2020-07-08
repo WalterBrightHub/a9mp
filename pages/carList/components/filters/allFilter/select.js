e7788bf31842ce294f39194a0428e42b7333bdf2
WalterBright <ssmbuild@outlook.com>
2020-07-09 01:05:31 +0800

    Style
    重排

--- pages/carList/components/filters/allFilter/select.js
+++ pages/carList/components/filters/allFilter/select.js
@@ -1,7 +1,6 @@
-const select = (sortField,isAsc) => carList => {
-
-      return carList.sort((a, b) => isAsc ? a[sortField] - b[sortField] : b[sortField] - a[sortField])
+const select = (sortField, isAsc) => carList => {
+  return carList.sort((a, b) => isAsc ? a[sortField] - b[sortField] : b[sortField] - a[sortField])
 }
 
-export const defaultSelect = select('rank',true)
+export const defaultSelect = select('rank', true)
 export default select
--- pages/carList/components/filters/brandFilter/select.js
+++ pages/carList/components/filters/brandFilter/select.js
@@ -1,6 +1,6 @@
 const select = brand => carList => {
-      return carList.filter(car => car.brand === brand)
-        .sort((a, b) => a.rank - b.rank)
+  return carList.filter(car => car.brand === brand)
+    .sort((a, b) => a.rank - b.rank)
 }
 
 export const defaultSelect = select('Lamborghini')
