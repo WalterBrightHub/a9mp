1d9f54659ee6eb897d5256881bd2a978a5262a2e
WalterBright <ssmbuild@outlook.com>
2020-10-28 01:47:34 +0800

    Style: Code

--- store/store.js
+++ store/store.js
@@ -1,33 +1,35 @@
 // https://ask.dcloud.net.cn/article/37468
 // 引入依赖  
-import Vue from 'vue'  
-import Vuex from 'vuex'  
-import createPersistedState from 'vuex-persistedstate'  
+import Vue from 'vue'
+import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
-Vue.use(Vuex)  
+Vue.use(Vuex)
 
-const state = {  
-  server:'gl'
-}  
+const state = {
+  server: 'gl'
+}
 
-export default new Vuex.Store({  
-  state,  
-  mutations:{
-    toggleServer(state){
-      const {server}=state
-      state.server=server==='gl'?'al':'gl'
+export default new Vuex.Store({
+  state,
+  mutations: {
+    toggleServer(state) {
+      const {
+        server
+      } = state
+      state.server = server === 'gl' ? 'al' : 'gl'
     }
   },
-  plugins: [  
+  plugins: [
     // 可以有多个持久化实例  
-    createPersistedState({  
-      key: 'app_config_data',  // 状态保存到本地的 key   
-      paths: ['server'],  // 要持久化的状态，在state里面取，如果有嵌套，可以  a.b.c   
-      storage: {  // 存储方式定义  
+    createPersistedState({
+      key: 'app_config_data', // 状态保存到本地的 key   
+      paths: ['server'], // 要持久化的状态，在state里面取，如果有嵌套，可以  a.b.c   
+      storage: { // 存储方式定义  
         getItem: (key) => uni.getStorageSync(key), // 获取  
         setItem: (key, value) => uni.setStorageSync(key, value), // 存储  
         removeItem: (key) => uni.removeStorageSync(key) // 删除  
-      }  
-    })  
-  ]  
+      }
+    })
+  ]
 })
