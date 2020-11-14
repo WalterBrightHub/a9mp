5ed17a3d62600d700c588637502f149b31ab9414
WalterBright <ssmbuild@outlook.com>
2020-11-14 23:37:30 +0800

    Feature: Use ClientDB

---
+++ pages/contest/components/contestItem.vue
@@ -0,0 +1,129 @@
+<template>
+  <view>
+    <view class="divider" v-if="index>0"></view>
+    <view class="card">
+      <view class="title-block">
+        <view class="contest-title" :class="'title-'+timeStatus">{{contest.contestName}}</view>
+        <view class="time-tip" :class="'tip-'+timeStatus">{{timeTip}}</view>
+      </view>
+
+      <event-detail-list :startTime="contest.startTime" :endTime="contest.endTime" :now="now" :mapName="contest.mapName"
+        :rewords="contest.rewords" />
+    </view>
+  </view>
+</template>
+
+<script>
+  import moment from 'moment'
+  import eventDetailList from './eventDetailList.vue'
+  moment.locale('zh-cn')
+  export default {
+    components: {
+      'event-detail-list': eventDetailList
+    },
+    props: ['contest', 'now', 'index'],
+    data() {
+      return {
+
+      };
+    },
+    computed: {
+      timeStatus(){
+        const {startTime,endTime}=this.contest
+        const {now}=this
+        return startTime>now?'feature':endTime>=now?'now':'past'
+      },
+      timeTip(){
+        
+          const {startTime,endTime}=this.contest
+          const {now}=this
+          return startTime>now?`${moment(startTime).to(this.now, true)}后开启`:endTime>=now?`${moment(endTime).from(this.now, true)}后结束`:'已结束'
+        if (startTime > this.now) {
+          return moment(startTime).to(this.now, true)
+        } else {
+          return moment(endTime).from(this.now, true)
+        }
+      }
+    },
+  }
+</script>
+
+<style lang="scss">
+  .divider {
+    padding-top: 3rpx;
+    background-color: $divider-color;
+
+    @include pad-devices {
+      padding-top: toPadPx(3);
+    }
+
+    @media (prefers-color-scheme: dark) {
+      background-color: $divider-color-dark;
+    }
+  }
+
+  .card {
+    padding: 20rpx;
+
+    @include pad-devices {
+      padding: toPadPx(20);
+    }
+  }
+
+  .title-block {
+    display: flex;
+  }
+
+  .contest-title {
+    font-size: 32rpx;
+    color: $text-title-color;
+    font-weight: bold;
+
+    @include pad-devices {
+      font-size: toPadPx(32);
+    }
+
+    @media (prefers-color-scheme: dark) {
+      color: $text-title-color-dark;
+    }
+  }
+
+  .time-tip {
+    margin-left: auto;
+
+  }
+
+  .title-future,
+  .tip-future {
+    color: $theme-color;
+
+    @media (prefers-color-scheme: dark) {
+      color: $theme-color-dark;
+    }
+  }
+
+  .title-now,
+  .tip-now {
+    color: #08a00b;
+  }
+
+  .title-past {
+    color: $text-title-color;
+
+    @media (prefers-color-scheme: dark) {
+      color: $text-title-color-dark;
+    }
+  }
+
+  .tip-reword {
+    color: #ffd800;
+  }
+
+  .tip-finish {
+    color: $text-help-color;
+
+    @media (prefers-color-scheme: dark) {
+      color: $text-help-color-dark;
+    }
+  }
+</style>
--- pages/contest/contest.vue
+++ pages/contest/contest.vue
@@ -3,143 +3,161 @@
     <view class="mode-changer">
       <view class="mode-button" @tap="onChangeMode">{{server==='al'?'国服':'国际'}} ⇌</view>
     </view>
+    <div class="contest-db">
+      <uni-clientdb class="cdb" ref="contestDB" v-slot:default="{data, pagination, loading, error, options}" :options="options"
+        collection="contest" orderby="startTime desc,contestName asc" :getone="false" :action="action" :where="where"
+        @load="onqueryload" @error="onqueryerror">
+        <view v-if="error" class="error">{{error.message}}</view>
+        <view v-else class="contest-list">
 
-    <request-fail v-if="contestStatus==='reject'" @onRetry='onRetry' />
-    <context v-else-if="contestStatus==='resolve'" :contestPresent='contestPresent' :contestPast='contestPast' :now="now" />
+          <contest-item :contest="contest" v-for="(contest, index) in data" :key="contest._id" class="contest-item"
+            :now="options.now" :index="index" />
+        </view>
+        <!-- <view v-if="loading" class="loading">加载中...</view> -->
+      </uni-clientdb>
+    </div>
   </view>
 </template>
 
 <script>
-  import requestFail from '../../components/requestFail/requestFail.vue'
-  import context from './context.vue'
-  
-    import {
-      mapState,
-      mapMutations
-    } from 'vuex'
-    
-  const {
-    myCloud
-  } = getApp().globalData
-  const db = myCloud.database();
-    const dbCmd = db.command;
-    const dbCollectionName = 'contest';
-  
-  const testClientDB=()=>{
-    db.collection('contest').count().then(res=>{
-      console.log(res)
-    }).catch(err=>{
-      console.log(err)
-    })
-  }
-  
-  let page = 0
-  const requestContestPresent = async function() {
-    return wx.cloud.callFunction({
-      name: 'getContest'
-    })
-  }
-  const requestContestPast = async function({
-    page,
-    limit
-  }) {
-    return uni.request({
-      url: 'https://a9cn.walterbright.cc/api/contest',
-      method: 'GET',
-      data: {
-        time: 'past',
-        page,
-        limit
-      }
-    })
-  }
+  import contestItem from './components/contestItem.vue'
+  import uniClientDB from '../../components/uni-clientdb/uni-clientdb.vue'
+
+  import {
+    mapState,
+    mapMutations
+  } from 'vuex'
+
+
+
+  // let page = 0
+  // const requestContestPresent = async function() {
+  //   return wx.cloud.callFunction({
+  //     name: 'getContest'
+  //   })
+  // }
+  // const requestContestPast = async function({
+  //   page,
+  //   limit
+  // }) {
+  //   return uni.request({
+  //     url: 'https://a9cn.walterbright.cc/api/contest',
+  //     method: 'GET',
+  //     data: {
+  //       time: 'past',
+  //       page,
+  //       limit
+  //     } 
+  //   })
+  // }
   export default {
     components: {
 
-      'request-fail': requestFail,
-      'context': context
+      'uni-clientdb': uniClientDB,
+      'contest-item': contestItem
     },
     data() {
       return {
-        contestStatus: 'ready',
-        contestPresent: [],
-        contestPast: [],
-        now: new Date().getTime()
+        // contestStatus: 'ready',
+        // contestPresent: [],
+        // contestPast: [],
+        now: new Date().getTime(),
+        action: '',
       }
     },
-    computed:{
-      
+    computed: {
+
       ...mapState(['server']),
+      options() {
+        return {
+          now: this.now
+        }
+      },
+      where() {
+        // console.log(this.now)
+        return '' //`server=='${this.server}'`
+      }
     },
     onLoad() {
-      this.contestStatus = 'pending'
+      // this.contestStatus = 'pending'
       uni.showLoading({
         title: '加载中'
       })
-      requestContestPresent()
-        .then((res) => {
-          const {
-            contest
-          } = res.result
-          this.contestStatus = 'resolve'
-          this.contestPresent = contest
-        }).catch(e => {
-          console.log(e)
-          this.contestStatus = 'reject'
-        }).finally(() => {
-          uni.hideLoading()
-        })
-      this.setContestPast()
+      // requestContestPresent()
+      //   .then((res) => {
+      //     const {
+      //       contest
+      //     } = res.result
+      //     this.contestStatus = 'resolve'
+      //     this.contestPresent = contest
+      //   }).catch(e => {
+      //     console.log(e)
+      //     this.contestStatus = 'reject'
+      //   }).finally(() => {
+      //     uni.hideLoading()
+      //   })
+      // this.setContestPast()
     },
     onPullDownRefresh() {
-      
-      testClientDB()
+
+      // testClientDB()
 
       this.now = new Date().getTime()
-      requestContestPresent()
-        .then((res) => {
-          const {
-            contest
-          } = res.result
-          this.contestPresent = contest
-          this.contestStatus = 'resolve'
-          uni.showToast({
-            title: '最新',
-            duration: 500,
-          })
-
-        }).catch(e => {
-          console.log(e)
-          this.contestStatus = 'reject'
-          uni.showToast({
-            title: '失败',
-            icon: 'none',
-            duration: 500
-          })
-        }).finally(() => {
-          uni.stopPullDownRefresh()
+      this.$refs.contestDB.loadData({
+        // clear: true
+      }, () => {
+        uni.stopPullDownRefresh()
+        uni.showToast({
+          title: '最新',
+          duration: 500,
         })
-      page = 0
-      this.setContestPast()
+      })
+      // requestContestPresent()
+      //   .then((res) => {
+      //     const {
+      //       contest
+      //     } = res.result
+      //     this.contestPresent = contest
+      //     this.contestStatus = 'resolve'
+      //     uni.showToast({
+      //       title: '最新',
+      //       duration: 500,
+      //     })
+
+      //   }).catch(e => {
+      //     console.log(e)
+      //     this.contestStatus = 'reject'
+      //     uni.showToast({
+      //       title: '失败',
+      //       icon: 'none',
+      //       duration: 500
+      //     })
+      //   }).finally(() => {
+      //     uni.stopPullDownRefresh()
+      //   })
+      // page = 0
+      // this.setContestPast()
     },
     onReachBottom() {
-      requestContestPast({
-        page,
-        limit: 20
-      }).then(([error, res]) => {
-        if (error) {
-          console.log(error)
-        } else {
-          page++
-          const {
-            data
-          } = res
-          if (data.status === 200) {
-            // console.log(data.contest)
-            this.contestPast = this.contestPast.concat(data.contest)
-          }
-        }
-      })
+
+      this.$refs.contestDB.loadMore()
+      // requestContestPast({
+      //   page,
+      //   limit: 20
+      // }).then(([error, res]) => {
+      //   if (error) {
+      //     console.log(error)
+      //   } else {
+      //     page++
+      //     const {
+      //       data
+      //     } = res
+      //     if (data.status === 200) {
+      //       // console.log(data.contest)
+      //       this.contestPast = this.contestPast.concat(data.contest)
+      //     }
+      //   }
+      // })
     },
     onShareAppMessage() {
 
@@ -170,52 +188,56 @@
         })
         this.toggleServer()
       },
-      setContestPast() {
-        requestContestPast({
-            page,
-            limit: 20
-          })
-          .then(([error, res]) => {
-            if (error) {
-              console.log(error)
-            } else {
-              page++
-              const {
-                data
-              } = res
-              if (data.status === 200) {
-                // console.log(data.contest)
-                this.contestPast = data.contest
-              }
-            }
-          })
-      },
-      onRetry() {
-        this.now = new Date().getTime()
-        uni.showLoading({
-          title: '重试中'
-        })
-        requestContestPresent()
-          .then((res) => {
-            const {
-              contest
-            } = res.result
+      // setContestPast() {
+      //   requestContestPast({
+      //       page,
+      //       limit: 20
+      //     })
+      //     .then(([error, res]) => {
+      //       if (error) {
+      //         console.log(error)
+      //       } else {
+      //         page++
+      //         const {
+      //           data
+      //         } = res
+      //         if (data.status === 200) {
+      //           // console.log(data.contest)
+      //           this.contestPast = data.contest
+      //         }
+      //       }
+      //     })
+      // },
+      // onRetry() {
+      //   this.now = new Date().getTime()
+      //   uni.showLoading({
+      //     title: '重试中'
+      //   })
+      //   requestContestPresent()
+      //     .then((res) => {
+      //       const {
+      //         contest
+      //       } = res.result
 
-            this.contestPresent = contest
-            this.contestStatus = 'resolve'
-            uni.hideLoading()
-          }).catch(e => {
-            console.log(e)
-            this.contestStatus = 'reject'
-            uni.showToast({
-              title: '失败',
-              duration: 500,
-              icon: 'none'
-            })
-          })
-        page = 0
-        this.setContestPast()
+      //       this.contestPresent = contest
+      //       this.contestStatus = 'resolve'
+      //       uni.hideLoading()
+      //     }).catch(e => {
+      //       console.log(e)
+      //       this.contestStatus = 'reject'
+      //       uni.showToast({
+      //         title: '失败',
+      //         duration: 500,
+      //         icon: 'none'
+      //       })
+      //     })
+      //   page = 0
+      //   this.setContestPast()
+      // },
+      onqueryload() {
+        uni.hideLoading()
       },
+      onqueryerror() {},
     }
   }
 </script>
@@ -230,7 +252,7 @@
 
     @include pad-devices {
       padding: toPadPx(20);
-    max-width: 768px;
+      max-width: 768px;
       margin: 0 auto;
     }
   }
@@ -258,6 +280,55 @@
     @media (prefers-color-scheme: dark) {
       background-color: $card-bg-color-dark;
       color: $theme-color-dark;
+    }
+  }
+
+  .contest-db {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 20rpx 20rpx 30rpx 20rpx;
+
+    @include pad-devices {
+      padding: toPadPx(20) toPadPx(20) toPadPx(30) toPadPx(20);
+    }
+  }
+
+  .contest-list {
+    background-color: $card-bg-color;
+    border-radius: 10rpx;
+    font-size: 28rpx;
+
+    @include pad-devices {
+      border-radius: toPadPx(10);
+      font-size: toPadPx(28);
+    }
+
+    @media (prefers-color-scheme: dark) {
+      background-color: $card-bg-color-dark;
+    }
+  }
+
+  .contest-list+.contest-list {
+    margin-top: 20rpx;
+
+    @include pad-devices {
+      margin-top: toPadPx(20);
+    }
+  }
+
+  .loading,
+  .error {
+    margin-top: 10rpx;
+    color: $text-help-color;
+    display: flex;
+    justify-content: center;
+
+    @include pad-devices {
+      margin-top: toPadPx(10);
+    }
+
+    @media (prefers-color-scheme: dark) {
+      color: $text-help-color-dark
     }
   }
 </style>
